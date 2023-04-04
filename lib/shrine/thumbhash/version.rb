659c40cd4b6482a7d25f0267f15039bcc354cdfd
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2023-04-04 20:23:32 +0900

    bundle gem

---
+++ lib/shrine/thumbhash/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Shrine
+  module Thumbhash
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/shrine/thumbhash_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Shrine::Thumbhash do
+  it "has a version number" do
+    expect(Shrine::Thumbhash::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
