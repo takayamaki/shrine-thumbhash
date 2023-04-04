6bf20ae5ba83d483b32459bc9a0b65ed01e7b54c
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2023-04-05 06:52:49 +0900

    Add spec for Shrine.generate_thumbhash
    Image from https://www.publicdomainpictures.net/en/view-image.php?image=328175&picture=horse-silhouette-at-sunrise

--- spec/thumbhash_spec.rb
+++ spec/thumbhash_spec.rb
@@ -1,11 +1,29 @@
 # frozen_string_literal: true
 
+require "bundler/setup"
+require "shrine"
+require "shrine/plugins/thumbhash"
+
 RSpec.describe Shrine::Plugins::Thumbhash do
   it "has a version number" do
-    expect(Shrine::Plugins::Thumbhash::VERSION).not_to be nil
+    expect(Shrine::Plugins::Thumbhash::VERSION).not_to be_nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let :shrine_class do
+    shrine_class = Class.new(Shrine)
+    shrine_class.class_eval { plugin :thumbhash }
+    shrine_class
+  end
+
+  def image
+    File.open("#{__dir__}/fixtures/horse_silhouette_at_sunrise.jpg", binmode: true)
+  end
+
+  describe "ClassMethod" do
+    describe "generate_thumbhash" do
+      it "Shrine.generate_thumbhash returns thumbhash from image" do
+        expect(shrine_class.generate_thumbhash(image)).to eq "4igaZIp2iHiPeHeGd3d2hFAmCA"
+      end
+    end
   end
 end
